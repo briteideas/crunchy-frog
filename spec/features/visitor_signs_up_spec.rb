45f051e52df880af49e3a00de4881d3e26ff15f2
ASAP <alexander.sapountzis@gmail.com>
2016-03-26 13:49:23 -0400

    Rebrand to couchbot.com :rocket:

--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -5,7 +5,9 @@ feature 'Visitor signs up' do
     visit sign_up_path
     expect(page).to have_field("user[email]")
     # expect(page).to have_field("user[password]")
-    expect(page).to have_content("An entertainment guide for your inbox.")
+    expect(page).to have_title("couchbot.com")
+    expect(page).to have_content("couchbot.com")
+    expect(page).to have_content("The weekly guide of the best ways to waste your time.")
 
     fill_in "user[email]", with: "bruce@spam.org"
     click_button "Give me emails!"
